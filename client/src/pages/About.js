763b53da51abc08a4f77353be8d10c9f617658fb
Lex Matthews <tanukiguernica@gmail.com>
2022-06-01 22:39:38 -0400

    wrote the rest of the componets, need to style the page and after that basically just flex

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,7 +3,20 @@ import React from 'react';
 const About= () =>{
     return (
         <main>
-
+            <div>
+                {/* img here */}
+            </div>
+            <div>
+                <p>
+                    Hi! My name is Lex and I'm a 21 year old junior full-stack web dev and computer science student.
+                    When I was six my parents bought me a first generation PlayStation,
+                    and that was the meet cute for me and programming.
+                    Ever since, I've been fascinated by art, technology, and any other inbetweens.
+                    I aim to create quality projects, adapting to whatever the situation calls, 
+                    and to envision creations that capture others the same way they did me.
+                </p>
+                <a href=''>Resume</a>
+            </div>
         </main>
     );
 }
--- client/src/pages/Work.js
+++ client/src/pages/Work.js
@@ -3,7 +3,22 @@ import React from 'react';
 const Work= () =>{
     return (
         <main>
-
+             <div>
+                <div>
+                    <img src=""></img>
+                    <div>
+                        <h3>gitGud</h3>
+                        <h4>HTML5/CSS/JS</h4>
+                    </div>
+                </div>
+                <div>
+                    <img src=""></img>
+                    <div>
+                        <h3>Savepoint</h3>
+                        <h4>HBS/Node.JS/MySQL</h4>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
