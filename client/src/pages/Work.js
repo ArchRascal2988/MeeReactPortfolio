6de1e24bfb98ac483fcba95e28a8291701e82cd4
Lex Matthews <tanukiguernica@gmail.com>
2022-06-03 10:47:06 -0400

    fixed img order

--- client/src/pages/Work.js
+++ client/src/pages/Work.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Project from '../componets/projectCard'
 
 const projNames= ["gitGud", "savepoint", "UT Browser Game", "UT", "UT"];
-const imgPaths=['https://i.imgur.com/a3C3hZm.jpg', 'https://i.imgur.com/FtKs4Bp.png', 'https://i.imgur.com/5B5eoWd.png', 'https://i.imgur.com/5B5eoWd.png', 'https://i.imgur.com/5B5eoWd.png'];
+const imgPaths=['https://i.imgur.com/FtKs4Bp.png', 'https://i.imgur.com/a3C3hZm.jpg', 'https://i.imgur.com/5B5eoWd.png', 'https://i.imgur.com/5B5eoWd.png', 'https://i.imgur.com/5B5eoWd.png'];
 const projLinks=['https://mysterious-atoll-10250.herokuapp.com/landing/login', 'https://canro2b.github.io/gitGud/']
 const langs= ["HTML/CSS, JS", "Handlebars/CSS, JS, SQL", 'wip', 'wip', 'wip']
 
